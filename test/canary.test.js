a21f38da711234cf87473bee7963b69306b2dd09
noisette <kasusky.music@gmail.com>
2020-07-24 20:42:53 -0700

    added database input test via mocha chai chai-http

--- test/canary.test.js
+++ test/canary.test.js
@@ -1,9 +1,38 @@
-var expect = require("chai").expect;
+// var expect = require("chai").expect;
 
-describe("canary test", function() {
-  // A "canary" test is one we set up to always pass
-  // This can help us ensure our testing suite is set up correctly before writing real tests
-  it("should pass this canary test", function() {
-    expect(true).to.be.true;
-  });
+// describe("canary test", function() {
+//   // A "canary" test is one we set up to always pass
+//   // This can help us ensure our testing suite is set up correctly before writing real tests
+//   it("should pass this canary test", function() {
+//     expect(true).to.be.true;
+//   });
+// });
+
+
+// 'use strict';
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+const { expect } = require('chai');
+
+chai.use(chaiHttp);
+
+describe('Inputting data into database', function() {
+    var host = "http://localhost:3000";
+    var path = "/users/add";
+
+    it('Info is in database', function(done) {
+        chai
+            .request(host)
+            .post(path)
+            // .field('myparam' , 'test')
+            // .set('content-type', 'application/x-www-form-urlencoded')
+            .send({name: 'danver', password: "password", allergies: "nuts", dietaryRestrictions: "fish"})
+            .then(function (res) {
+              expect(res).to.have.status(200);
+              done();
+            })
+            .catch(function (err) {
+              throw err;
+            });
+    });
 });
