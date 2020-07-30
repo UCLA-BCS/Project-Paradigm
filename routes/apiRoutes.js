d94c774033c36750f919da4f20729ff857a6cf79
Mark F <the.mark.fullton@gmail.com>
2020-07-29 22:05:47 -0700

    Added api route to get the id of the session cookie

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,24 @@
 var db = require("../models");
 
-module.exports = function (app) {
+module.exports = function(app) {
   //========================================================================================
   // USER RELATED FUNCTIONS
   //========================================================================================
   // Returns all users in database
   // Should this be used...?
-  app.get("/users/all", function (req, res) {
-    db.SiteUser.findAll({}).then(function (bestow_db) {
+  app.get("/users/all", function(req, res) {
+    db.SiteUser.findAll({}).then(function(bestow_db) {
       res.json(bestow_db);
     });
   });
 
+  // Gives you the current user id
+  app.get("/curr-user-id", function(req, res) {
+    res.json(req.session.userId);
+  });
+
   // Deletes a user given the id
-  app.delete("/users/delete/:userID", function (req, res) {
+  app.delete("/users/delete/:userID", function(req, res) {
     var deleteUser = req.params.userID;
 
     db.SiteUser.destroy({
@@ -21,30 +26,30 @@ module.exports = function (app) {
         id: deleteUser,
       },
     })
-      .then(function (bestow_db) {
+      .then(function(bestow_db) {
         res.json(bestow_db);
       })
-      .catch(function (err) {
+      .catch(function(err) {
         res.json(err);
       });
   });
 
   // Searches for all drinks associated with the user in question
   // Requires userID--not user name
-  app.get("/users/drinks/:userID", function (req, res) {
+  app.get("/users/drinks/:userID", function(req, res) {
     var queryUser = req.params.userID;
 
     db.SiteDrink.findAll({
       where: {
         owner: queryUser,
       },
-    }).then(function (bestow_db) {
+    }).then(function(bestow_db) {
       res.json(bestow_db);
     });
   });
 
   // Deletes a drink given the id
-  app.delete("/users/delete-drinks/:drinkID", function (req, res) {
+  app.delete("/users/delete-drinks/:drinkID", function(req, res) {
     var deleteDrink = req.params.drinkID;
 
     db.SiteDrink.destroy({
@@ -52,16 +57,16 @@ module.exports = function (app) {
         id: deleteDrink,
       },
     })
-      .then(function (bestow_db) {
+      .then(function(bestow_db) {
         res.json(bestow_db);
       })
-      .catch(function (err) {
+      .catch(function(err) {
         res.json(err);
       });
   });
 
   // Updates drink given the id
-  app.put("/users/update-drinks/:drinkID", function (req, res) {
+  app.put("/users/update-drinks/:drinkID", function(req, res) {
     var updateDrink = req.params.drinkID;
 
     db.SiteDrink.update(
@@ -77,17 +82,17 @@ module.exports = function (app) {
         },
       }
     )
-      .then(function (bestow_db) {
+      .then(function(bestow_db) {
         res.json(bestow_db);
       })
-      .catch(function (err) {
+      .catch(function(err) {
         res.json(err);
       });
   });
 
   // Adds drink with the userID given
   // DO NOT USE USER NAME (get drinks function searches by ID)
-  app.post("/users/add-drinks/:userID", function (req, res) {
+  app.post("/users/add-drinks/:userID", function(req, res) {
     var queryUser = req.params.userID;
 
     db.SiteDrink.create({
@@ -97,10 +102,10 @@ module.exports = function (app) {
       drinkName: req.body.drinkName,
       specialInstructions: req.body.specialInstructions,
     })
-      .then(function (bestow_db) {
+      .then(function(bestow_db) {
         res.json(bestow_db);
       })
-      .catch(function (err) {
+      .catch(function(err) {
         res.json(err);
       });
   });
@@ -108,24 +113,24 @@ module.exports = function (app) {
   // Adds a new user to the database
   // Password should be hashed PRIOR TO SENDING (hence it is not hashed here)
   //     This is because the information is sent via post; hence, the password should be hashed before sending
-  app.post("/users/add", function (req, res) {
+  app.post("/users/add", function(req, res) {
     db.SiteUser.create({
       name: req.body.name,
       password: req.body.password,
       allergies: req.body.allergies,
       dietaryRestrictions: req.body.dietaryRestrictions,
     })
-      .then(function (bestow_db) {
+      .then(function(bestow_db) {
         res.json(bestow_db);
       })
-      .catch(function (err) {
+      .catch(function(err) {
         res.json(err);
       });
   });
 
   // Takes one user and initiates a friendship with another user.
   // Uses the user IDs of both users to do this.
-  app.post("/friend/:userOne/:userTwo", function (req, res) {
+  app.post("/friend/:userOne/:userTwo", function(req, res) {
     var userOne = req.params.userOne;
     var userTwo = req.params.userTwo;
 
@@ -133,16 +138,16 @@ module.exports = function (app) {
       friendInitiator: userOne,
       friendReceiver: userTwo,
     })
-      .then(function (bestow_db) {
+      .then(function(bestow_db) {
         res.json(bestow_db);
       })
-      .catch(function (err) {
+      .catch(function(err) {
         res.json(err);
       });
   });
 
   // Deletes a friendship given the ids of two users
-  app.delete("/delete-friend/:userOne/:userTwo", function (req, res) {
+  app.delete("/delete-friend/:userOne/:userTwo", function(req, res) {
     var userOne = req.params.userOne;
     var userTwo = req.params.userTwo;
 
@@ -152,23 +157,23 @@ module.exports = function (app) {
         friendReceiver: userTwo,
       },
     })
-      .then(function (bestow_db) {
+      .then(function(bestow_db) {
         res.json(bestow_db);
       })
-      .catch(function (err) {
+      .catch(function(err) {
         res.json(err);
       });
   });
 
   // Returns all frieds of the userID given
-  app.get("/get-friends/:userID", function (req, res) {
+  app.get("/get-friends/:userID", function(req, res) {
     var userID = req.params.userID;
 
     db.SiteFriends.findAll({
       where: {
         friendInitiator: userID,
       },
-    }).then(function (bestow_db) {
+    }).then(function(bestow_db) {
       res.json(bestow_db);
     });
   });
